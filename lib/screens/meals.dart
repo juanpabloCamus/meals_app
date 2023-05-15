8963aae9abdfef07cb1918e14e131e57da16c2a6
Juan Pablo <juanpablo@meowtel.com>
2023-05-15 14:43:31 -0300

    navigaotr

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -14,6 +14,7 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
+      itemCount: meals.length,
       itemBuilder: ((ctx, index) => Text(meals[index].title)),
     );
 
@@ -39,9 +40,10 @@ class MealsScreen extends StatelessWidget {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: content);
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
